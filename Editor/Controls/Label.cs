9243703a187dc0b38a8a9a4487321e0b8c939d2c
Ricardo Amores <rickyah@gmail.com>
2014-03-17 18:28:57 +0100

    Add default constructors to Layer

--- Editor/Controls/Label.cs
+++ Editor/Controls/Label.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 namespace UnityForms
 {
-    
     public class Label : Control
     {
+        public Label(string text) : base(text)
+        {
+        }
+
+        public Label(string text, Control parent) : base(text, parent)
+        {
+        }
+
         protected override void OnPaint()
         {
             GUILayout.Label(this.Text);
         }
-    } 
+    }
 }
