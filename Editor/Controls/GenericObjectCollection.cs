cbbe9923229b53774a4be427d875bcf4f68130dc
Ricardo Amores <rickyah@gmail.com>
2014-03-17 18:27:11 +0100

    OnChange event now notifies if the item collection has been changed

--- Editor/Controls/GenericObjectCollection.cs
+++ Editor/Controls/GenericObjectCollection.cs
@@ -13,54 +13,66 @@ namespace UnityForms
     {
         public GenericObjectCollectionEventArgs(T thatObject)
         {
-            this.Object= thatObject;
+            this.Object = thatObject;
         }
-                
-        public T Object {get; private set;}
+
+        public T Object { get; private set; }
     }
-    
+
     public class GenericObjectCollection<T> : IList<T>, ICollection<T>, IEnumerable<T>
     {
         private List<T> _dataContainer = new List<T>();
         private object _dataContainerSyncRoot = new object();
-        
+
         #region Events
+
         public event EventHandler<GenericObjectCollectionEventArgs<T>> Added;
         public event EventHandler Cleared;
-        public event EventHandler<GenericObjectCollectionEventArgs<T>> Changed;
+        public event EventHandler Changed;
         public event EventHandler<GenericObjectCollectionEventArgs<T>> Removed;
+
         #endregion
 
         #region OnEvents
+
         protected virtual void OnAdded(T addedObject)
         {
-            if (Added != null) Added(this, new GenericObjectCollectionEventArgs<T>(addedObject));
-            OnChanged( addedObject );
+            if (Added != null)
+                Added(this, new GenericObjectCollectionEventArgs<T>(addedObject));
+            OnChanged();
         }
 
         protected virtual void OnCleared()
         {
-            if (Cleared != null) Cleared(this, EventArgs.Empty);
+            if (Cleared != null)
+                Cleared(this, EventArgs.Empty);
+            
+            OnChanged();
         }
-        
-        protected virtual void OnChanged(T changedObject)
+
+        protected virtual void OnChanged()
         {
-            if (Changed != null) Changed(this, new GenericObjectCollectionEventArgs<T>(changedObject));
+            if (Changed != null)
+                Changed(this, EventArgs.Empty);
         }
 
         protected virtual void OnRemoved(T removedObject)
         {
-            if (Removed != null) Removed(this, new GenericObjectCollectionEventArgs<T>(removedObject));
-            OnChanged( removedObject);
+            if (Removed != null)
+                Removed(this, new GenericObjectCollectionEventArgs<T>(removedObject));
+            
+            OnChanged();
         }
-        
+
         #endregion
+
         #region IList implementation
+
         public void Add(T value)
         {
-            this.Add(new T[] {value});
+            this.Add(new T[] { value });
         }
-        
+
         public void Add(IEnumerable<T> values)
         {
             foreach (var value in values)
@@ -69,28 +81,29 @@ namespace UnityForms
                 OnAdded(value);
             }
         }
-        
+
         public void Clear()
         {
             _dataContainer.Clear();
             OnCleared();
         }
+
         public bool Contains(T value)
         {
             return _dataContainer.Contains(value);
         }
-        
+
         public int IndexOf(T value)
         {
             return _dataContainer.IndexOf(value);
         }
-        
+
         public void Insert(int index, T value)
         {
             _dataContainer.Insert(index, value);
             OnAdded(value);
         }
-        
+
         public bool Remove(T value)
         {
             var result = _dataContainer.Remove(value);
@@ -101,12 +114,14 @@ namespace UnityForms
             
             return result;
         }
+
         public void RemoveAt(int index)
         {
             var value = this[index];
             _dataContainer.RemoveAt(index);
             OnRemoved(value);
         }
+
         public bool IsFixedSize
         {
             get
@@ -114,6 +129,7 @@ namespace UnityForms
                 return false;
             }
         }
+
         public bool IsReadOnly
         {
             get
@@ -121,6 +137,7 @@ namespace UnityForms
                 return false;
             }
         }
+
         public T this[int index]
         {
             get
@@ -129,26 +146,24 @@ namespace UnityForms
             }
             set
             {
-                var oldValue = _dataContainer[index];
                 _dataContainer[index] = value;
-                
-                if (!oldValue.Equals(value))
-                {
-                    OnChanged(value);
-                }
             }
         }
+
         #endregion
+
         #region ICollection implementation
 
         public void CopyTo(T[] array, int arrayIndex)
         {
             Array.Copy(_dataContainer.ToArray(), array, arrayIndex);
         }
+
         public void CopyTo(Array array, int index)
         {
             CopyTo(array, index);
         }
+
         public int Count
         {
             get
@@ -156,6 +171,7 @@ namespace UnityForms
                 return _dataContainer.Count;
             }
         }
+
         public bool IsSynchronized
         {
             get
@@ -167,6 +183,7 @@ namespace UnityForms
                 
             }
         }
+
         public object SyncRoot
         {
             get
@@ -174,19 +191,25 @@ namespace UnityForms
                 return _dataContainerSyncRoot;
             }
         }
+
         #endregion
+
         #region IEnumerable implementation
+
         public IEnumerator<T> GetEnumerator()
         {
             return _dataContainer.GetEnumerator();
         }
+
         #endregion
+
         #region IEnumerable implementation
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
+
         #endregion
     }
-    
 }
